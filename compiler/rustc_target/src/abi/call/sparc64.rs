012d48147b4bfc37b694e3d9fcac1ddfa760c378
Petr Sumbera <petr.sumbera@oracle.com>
2021-10-12 09:21:47 +0200

    Also returning arguments need similar care.

--- compiler/rustc_target/src/abi/call/sparc64.rs
+++ compiler/rustc_target/src/abi/call/sparc64.rs
@@ -43,6 +43,81 @@ where
         ret.cast_to(uniform);
         return;
     }
+
+    let mut has_8_or_16_bits = false;
+
+    let mut prefix = [None; 8];
+    let mut prefix_index = 0;
+
+    match ret.layout.fields {
+        abi::FieldsShape::Primitive |
+        abi::FieldsShape::Array { .. } |
+        abi::FieldsShape::Union(_) => { }
+
+        // Handle structures with floats (can we have here structure without float, guess not)
+        abi::FieldsShape::Arbitrary { .. } => {
+
+            let mut has_float = false;
+            let mut size = 0;
+
+            for i in 0..ret.layout.fields.count() {
+
+                let field = ret.layout.field(cx, i);
+
+                if let abi::Abi::Scalar(scalar) = &field.abi {
+                    if let abi::F32 = scalar.value {
+                        size += 4;
+                        has_float = true;
+                        prefix[prefix_index] = Some(RegKind::Float);
+                        prefix_index += 1;
+                    } else if let abi::F64 = scalar.value {
+                        size += 8;
+                        has_float = true;
+                        prefix[prefix_index] = Some(RegKind::Float);
+                        prefix_index += 1;
+                    } else if let abi::Int(i, _signed) = scalar.value {
+                        //size += i.size().bits()/8;
+                        // one register is used to pass more i8/i16 types
+                        if has_8_or_16_bits == false {
+                            prefix[prefix_index] = Some(RegKind::Integer);
+                            prefix_index += 1;
+                        }
+                        if i.size().bits() == 8 || i.size().bits() == 16 {
+                          has_8_or_16_bits = true;
+                        }
+                    } else if let abi::Pointer = scalar.value {
+                        // should care about 8 or 16 bits too?
+                        size += 8;
+                    }
+                }
+            }
+
+            // bigger structures will be passed indirecrtly
+            if has_float && size <= 16 {
+
+                // special case needs i8 and i16 types
+                if has_8_or_16_bits {
+                     ret.cast_to(CastTarget {
+                                prefix,
+                                prefix_chunk_size: Size::from_bytes(8),
+                                rest: Uniform { unit: Reg::i64(), total: Size::from_bytes(0) },
+                     });
+                     return;
+                 }
+
+                 // to pass in registers InReg flag need to be set
+                 if let PassMode::Direct(ref mut attrs) = ret.mode {
+                     attrs.set(ArgAttribute::InReg);
+                     return;
+                 } else if let PassMode::Pair(ref mut attrs1, ref mut attrs2) = ret.mode {
+                     attrs1.set(ArgAttribute::InReg);
+                     attrs2.set(ArgAttribute::InReg);
+                     return;
+                 }
+             }
+        }
+    };
+
     let size = ret.layout.size;
     let bits = size.bits();
     if bits <= 256 {
